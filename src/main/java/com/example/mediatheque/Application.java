01920aba8ae1603013cba8aef032e9b8fb79c001
Oumaima ouchlih <oumaimaouchlih22@gmail.com>
2024-12-23 17:47:05 +0100

    Supprimer le sous-dépôt ajouté par erreur

--- src/main/java/com/example/mediatheque/Application.java
+++ src/main/java/com/example/mediatheque/Application.java
@@ -4,7 +4,7 @@ public class Application {
     public static void main(String[] args) {
         Bibliothèque bibliothèque = new Bibliothèque();
         bibliothèque.ajouterLivre(new Livre("1984", "George Orwell"));
-        bibliothèque.ajouterLivre(new Livre("Le Petit Prince", "Antoine de Saint-Exupéry"));
+        bibliothèque.ajouterLivre(new Livre("Le Petit Prince !", "Antoine de Saint-Exupéry"));
 
         for (Livre livre : bibliothèque.listerLivres()) {
             System.out.println(livre);
